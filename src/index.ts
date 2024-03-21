7334216c6cd55bc18cba9724d6089ebcb66f2a94
Laurens Kruis <laurens@kruis.name>
2024-03-21 11:46:00 +0100

    Created a basic hello world webserver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import express, {Request, Response, Express } from "express";
+import { AddressInfo } from 'net'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app : Express = express();
+const SERVICE_PORT = process.env.SERVICE_PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+const server = app.listen(SERVICE_PORT, () => {
+    const addr : AddressInfo = server.address() as AddressInfo;
+    console.log(`Listening on http://localhost:${addr.port}`);
+});
