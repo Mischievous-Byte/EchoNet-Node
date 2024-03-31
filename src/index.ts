13dbde3c84b7bf721478a41b7b541e0ace114330
Laurens Kruis <laurens@kruis.name>
2024-03-31 15:44:12 +0200

    Removed unused code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express, {Request, Response, Express } from "express";
 import { AddressInfo } from 'net'
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app : Express = express();
-const SERVICE_PORT = process.env.SERVICE_PORT || 3000;
+const SERVICE_PORT = 3000;
 
 app.get("/ping", (req: Request, res: Response) => {
     res.send("Pong!");
-})
+});
 
 const server = app.listen(SERVICE_PORT, () => {
     const addr : AddressInfo = server.address() as AddressInfo;
